6647a626675aab485b9b77c91a66985cb0ab2af7
RolandMario <rolandmario2@gmail.com>
2022-06-23 15:30:36 +0100

    deleting files

--- client/src/components/constants/Constants.js
+++ client/src/components/constants/Constants.js
@@ -1,2 +1,2 @@
-//export const baseURL =  "http://176.58.122.154:8000"
-export const baseURL =  "http://localhost:8000"
+export const baseURL =  "http://176.58.122.154:8000"
+//export const baseURL =  "http://localhost:8000"
